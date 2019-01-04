f7ed7c3a2ed77a38ff711515553f5eaec7967457
Victor Galan <victor.galan@liferay.com>
2019-01-04 11:42:55 +0100

    Format

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,52 @@
 <template>
-	<div class="hello">
-		<ul>
-			<li v-for="meetup in meetups" v-bind:key="meetup.title">
-				{{meetup.title}}
-			</li>
-		</ul>
-	</div>
+  <div class="hello">
+    <ul>
+      <li v-for="meetup in meetups" :key="meetup.title">{{meetup.title}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-
-    export default {
-        name: 'HelloWorld',
-        mounted() {
-            this.fetchItems()
-        },
-        methods: {
-            fetchItems() {
-                fetch(`https://liferay-liferayapio.wedeploy.io/o/api/content-space/20126/structured-contents`)
-                    .then(stream => stream.json())
-                    .then(data => this.meetups = data._embedded.StructuredContent)
-                    .catch(error => console.error(error))
-            }
-        },
-        data: function () {
-            return {
-                meetups: []
-            }
-        },
-        props: {
-            msg: String,
-        }
+export default {
+  name: "HelloWorld",
+  data: function() {
+    return {
+      meetups: []
     };
+  },
+  mounted() {
+    this.fetchItems();
+  },
+  methods: {
+    fetchItems() {
+      fetch(
+        `https://liferay-liferayapio.wedeploy.io/o/api/content-space/20126/structured-contents`
+      )
+        .then(stream => stream.json())
+        .then(data => (this.meetups = data._embedded.StructuredContent))
+        .catch(error => console.error(error));
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	h3 {
-		margin: 40px 0 0;
-	}
+h3 {
+  margin: 40px 0 0;
+}
 
-	ul {
-		list-style-type: none;
-		padding: 0;
-	}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-	li {
-		display: inline-block;
-		margin: 0 10px;
-	}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-	a {
-		color: #42b983;
-	}
+a {
+  color: #42b983;
+}
 </style>
